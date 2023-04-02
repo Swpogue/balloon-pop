ef1ff678aea5a983bb0e820965e3a78d56b8e0ee
Swpogue <swpogue02@gmail.com>
2023-04-02 16:23:06 -0600

    format

--- main.js
+++ main.js
@@ -14,31 +14,31 @@ let clockId = 0
 let timeRemaining = 0
 let currentPlayer = {}
 let currentColor = "red"
-let possibleColors =["red", "green", "blue", "purple", "pink"]
+let possibleColors = ["red", "green", "blue", "purple", "pink"]
 
-function startGame(){
+function startGame() {
   document.getElementById("game-controls").classList.remove("hidden")
   document.getElementById("main-controls").classList.add("hidden")
   document.getElementById("scoreboard").classList.add("hidden")
   startClock()
   setTimeout(stopGame, gameLength)
-  }
+}
 
-function startClock(){
+function startClock() {
   timeRemaining = gameLength
   drawClock()
   clockId = setInterval(drawClock, 1000)
 }
 
-function stopClock (){
+function stopClock() {
   clearInterval(clockId)
 }
 
-function drawClock(){
+function drawClock() {
   let countdownElem = document.getElementById("countdown")
   countdownElem.innerText = (timeRemaining / 1000).toString()
   timeRemaining -= 1000
-  
+
 }
 
 function inflate() {
@@ -49,10 +49,10 @@ function inflate() {
   draw()
 }
 
-function checkBalloonPop(){
+function checkBalloonPop() {
   if (height >= maxSize) {
     console.log("pop the balloon")
-    let  balloonElement = document.getElementById("balloon")
+    let balloonElement = document.getElementById("balloon")
     balloonElement.classList.remove(currentColor)
     getRandomColor()
     balloonElement.classList.add(currentColor)
@@ -62,14 +62,14 @@ function checkBalloonPop(){
   }
 }
 
-function getRandomColor(){
+function getRandomColor() {
   let i = Math.floor(Math.random() * possibleColors.length);
   currentColor = possibleColors[i]
-  
+
 }
 
-function draw(){
-  let  balloonElement = document.getElementById("balloon")
+function draw() {
+  let balloonElement = document.getElementById("balloon")
   let clickCountElem = document.getElementById("click-count")
   let popCountElem = document.getElementById("pop-count")
   let highPopCountElem = document.getElementById("high-pop-count")
@@ -78,25 +78,25 @@ function draw(){
 
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
-  
+
   clickCountElem.innerText = clickCount.toString()
   popCountElem.innerText = currentPopCount.toString()
   highPopCountElem.innerText = currentPlayer.topScore.toString()
 
   playerNameElem.innerText = currentPlayer.name
 }
-function stopGame(){
+function stopGame() {
   console.log("the game is over")
-  
+
   document.getElementById("main-controls").classList.remove("hidden")
   document.getElementById("game-controls").classList.add("hidden")
   document.getElementById("scoreboard").classList.remove("hidden")
-  
+
   clickCount = 0
   height = 120
   width = 100
 
-  if (currentPopCount > currentPlayer.topScore){
+  if (currentPopCount > currentPlayer.topScore) {
     currentPlayer.topScore = currentPopCount
     savePlayers()
   }
@@ -112,7 +112,7 @@ function stopGame(){
 let players = []
 loadPlayers()
 
-function setPlayer(event){
+function setPlayer(event) {
   event.preventDefault()
   let form = event.target
 
@@ -124,31 +124,31 @@ function setPlayer(event){
     currentPlayer = { name: playerName, topScore: 0 }
     players.push(currentPlayer)
     savePlayers()
-}
-    form.reset()
-    document.getElementById("game").classList.remove("hidden")
-    form.classList.add("hidden")
-    draw()
-    drawScoreboard()
+  }
+  form.reset()
+  document.getElementById("game").classList.remove("hidden")
+  form.classList.add("hidden")
+  draw()
+  drawScoreboard()
 }
 
-function changePlayer(){
+function changePlayer() {
   document.getElementById("player-form").classList.remove("hidden")
   document.getElementById("game").classList.add("hidden")
 }
 
-function savePlayers(){
+function savePlayers() {
   window.localStorage.setItem("players", JSON.stringify(players))
 }
-function loadPlayers(){
+function loadPlayers() {
   let playersData = JSON.parse(window.localStorage.getItem("players"))
-  
-  if(playersData) {
+
+  if (playersData) {
     players = playersData
   }
 }
 
-function drawScoreboard(){
+function drawScoreboard() {
   let template = ""
 
   players.sort((p1, p2) => p2.topScore - p1.topScore)
@@ -162,7 +162,7 @@ function drawScoreboard(){
     <span>score: ${player.topScore}</span>
   </div>
   `
-})
+  })
 
   document.getElementById("players").innerHTML = template
 
